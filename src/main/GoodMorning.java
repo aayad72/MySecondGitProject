8765ebfd14457d3b88b85c9b1fe1a01ce3abb358
aayad72 <33642806+aayad72@users.noreply.github.com>
2018-03-10 12:21:16 -0500

    added print statement

--- src/main/GoodMorning.java
+++ src/main/GoodMorning.java
@@ -5,6 +5,7 @@ public class GoodMorning {
 	public static void main(String[] args) {
 		
 		System.out.println("Good morning Class!");
+		System.out.println("My name is maria");
 		
 		/*
 		 * Git - create local repository
